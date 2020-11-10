af31f4ee1eef835060fe2805cc98c08fa87ccedc
Mathias Gout <mathias.gout@gmail.com>
2020-11-10 13:50:12 +0100

    list of problematic files

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class ImpotAssmat:
 
     MAIN_PATH = os.path.dirname(os.path.realpath(__file__))
     PDF_FOLDER_PATH = os.path.join(MAIN_PATH, NOM_DOSSIER_PDF)
+    CHECK_ERROR = False
 
     def __init__(self):
 
@@ -42,41 +43,49 @@ class ImpotAssmat:
             text_list = re.split(r'\n{1,}', text)
 
             # Info extraction
-            for i, item in enumerate(text_list):
-                text_list[i] = re.compile(r"\s+").sub(" ", item).strip()
-                
-                if "nombre d'heures normales" in text_list[i]:
-                    jour = float(text_list[i].split()[3].replace(",", "."))/8
-                elif "référence employeur" in text_list[i]:
-                    employeur = text_list[i+1].upper() 
-                elif "csg + rds non deductible de l'impot sur le revenu" in text_list[i]:
-                    csg = float(text_list[i].split()[12].replace(",", "."))
-                elif "salaire net déclaré (y compris indemnités)" in text_list[i]:
-                    salaire_net = float(text_list[i].split()[6].replace(",", "."))
+            try:
+                for i, item in enumerate(text_list):
+                    text_list[i] = re.compile(r"\s+").sub(" ", item).strip()                    
+                    
+                    if "nombre de jours d'activité" in text_list[i] and text_list[i][0] == "n":
+                        jour = float(text_list[i].split()[4].replace(",", "."))
+                    elif "référence employeur" in text_list[i]:
+                        employeur = text_list[i+1].upper() 
+                    elif "csg + rds non deductible de l'impot sur le revenu" in text_list[i]:
+                        csg = float(text_list[i].split()[12].replace(",", "."))
+                    elif "salaire net déclaré (y compris indemnités)" in text_list[i]:
+                        salaire_net = float(text_list[i].split()[6].replace(",", "."))                    
 
-            if employeur not in self.result_dict:
-                self.result_dict[employeur] = dict()
-                self.result_dict[employeur]["jour_tot"] = jour
-                self.result_dict[employeur]["csg_tot"] = csg
-                self.result_dict[employeur]["salaire_net_tot"] = salaire_net
-            else:
-                self.result_dict[employeur]["jour_tot"] += jour
-                self.result_dict[employeur]["csg_tot"] += csg
-                self.result_dict[employeur]["salaire_net_tot"] += salaire_net
+                if employeur not in self.result_dict:
+                    self.result_dict[employeur] = dict()
+                    self.result_dict[employeur]["jour_tot"] = jour
+                    self.result_dict[employeur]["csg_tot"] = csg
+                    self.result_dict[employeur]["salaire_net_tot"] = salaire_net
+                else:
+                    self.result_dict[employeur]["jour_tot"] += jour
+                    self.result_dict[employeur]["csg_tot"] += csg
+                    self.result_dict[employeur]["salaire_net_tot"] += salaire_net                
+
+            except:
+                print("Problème : {}".format(pdf))
+                self.CHECK_ERROR = True 
     
     def save_results(self):
         """ Save results in resultats.txt """
         
-        with open(os.path.join(self.MAIN_PATH, "resultats.txt"), "w") as f:
-            salaire_tot = 0
-            for employeur in self.result_dict:
-                salaire_employeur_tot = round(self.result_dict[employeur]["salaire_net_tot"] + self.result_dict[employeur]["csg_tot"] - 
-                                              self.result_dict[employeur]["jour_tot"]*3*SMIC, 2)
-                f.write("{} : {} €\n".format(employeur, salaire_employeur_tot))
-                salaire_tot += salaire_employeur_tot
-            
-            f.write("\n\n")
-            f.write("TOTAL ANNÉE: {} €".format(round(salaire_tot, 2)))
+        if self.CHECK_ERROR:
+            print("\nVérifier les fichiers ci-desssus.")
+        else:
+            with open(os.path.join(self.MAIN_PATH, "resultats.txt"), "w") as f:
+                salaire_tot = 0
+                for employeur in self.result_dict:
+                    salaire_employeur_tot = round(self.result_dict[employeur]["salaire_net_tot"] + self.result_dict[employeur]["csg_tot"] - 
+                                                self.result_dict[employeur]["jour_tot"]*3*SMIC, 2)
+                    f.write("{} : {} €\n".format(employeur, salaire_employeur_tot))
+                    salaire_tot += salaire_employeur_tot
+                
+                f.write("\n\n")
+                f.write("TOTAL ANNÉE: {} €".format(round(salaire_tot, 2)))
             
 
 if __name__ == "__main__":
